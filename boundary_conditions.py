753bad3c34b4a922191c6ab138b8842630231ad2
CarloPa <54579322+Carlopasquinucci@users.noreply.github.com>
2020-09-03 12:18:48 +0200

    Issue with the folder name solved
    modify_bc("constant/polyMesh/boundary") was 
    
    modify_bc("constant/polymesh/boundary")

--- boundary_conditions.py
+++ boundary_conditions.py
@@ -4,6 +4,10 @@
 These functions offer easy ways to automate changes in OpenFOAM settings by 
 modification of 'constant', 'system', '0' files and other files that define an 
 OpenFOAM case.
+
+Run as python boundary_conditions.py
+
+Checked with python3.7
 """
 
 import os
@@ -11,14 +15,11 @@ import sys
 
 version=1
 subversion=0
-"""
+
 if [[ $1 == "-help" ]]; then
 	echo "Version $version . $subversion"
 	echo "-version for version number"
 	echo "-help to run the help"
-	echo "-checkMesh to run the checkMesh"
-	echo "-checkMesh -parallel nprocessor to run the checkMesh in parallel"
-	echo "-verbose to maintain all the log file stored"
 exit 1
 fi
 
@@ -26,7 +27,7 @@ if [[ $1 == "-version" ]]; then
 	echo "Version $version . $subversion"
 exit 1
 fi
-"""
+
 # constant boundary patch replace
 # if name starts with
 # in--> patch
@@ -43,9 +44,6 @@ def read_file(file_name):
   return content
 
 
-#'foo' in '**foo**'
-
-
 def modify_bc(file_path):
   file_contents = read_file(file_path)
   section_flag = False
@@ -56,31 +54,30 @@ def modify_bc(file_path):
         if 'inlet' in line:
             section_flag = True
             new_bc="patch"
-            #print(str('inlet')+line)
+            
         if 'outlet' in line:
             section_flag = True
             new_bc="patch"
-            #print(str('outlet')+line)
+            #
         if ' wall' in line:
             section_flag = True
             new_bc="wall"
-            #print(str('wall'))
+           
         if 'symmetry' in line:
             section_flag = True
             new_bc="symmetry"
             print(str('symmetry')+line)
         if section_flag and "type" in line:
            line = line.replace("patch", new_bc)
- #       if section_flag and "physicalType" in line:
- #          line = line.replace("patch", new_bc)
+
         if "}" in line and section_flag == True:
            section_flag = False
-        #fil.write(line)
+
         f.write(line)
 
-  #fil.close()
+
   f.close()
   return
 
 
-modify_bc("constant/polymesh/boundary")
+modify_bc("constant/polyMesh/boundary")
